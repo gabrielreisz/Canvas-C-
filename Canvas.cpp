e81f816ff3c2a51ec46790ad4533b5e0d45dae59
Gabriel Costa Reis <gcostareis02@gmail.com>
2025-09-18 20:37:31 +0000

    Inicio usando Class e funções fora da Class

--- Canvas.cpp
+++ Canvas.cpp
@@ -1,9 +1,7 @@
 #include "Canvas.hpp"
-
-std::string GetColorCode(char cor)
-{
-  switch (cor)
-  {
+#include <iostream>
+std::string GetColorCode(char cor) {
+  switch (cor) {
   case 'K':
     return "\033[30m"; // Black
   case 'R':
@@ -25,57 +23,51 @@ std::string GetColorCode(char cor)
   }
 }
 
-Canva::Canva()
-{
-  _pixels = nullptr;
-  _cores = nullptr;
+Canva::Canva() {
+  pixels = nullptr;
+  cores = nullptr;
 }
-Canva::~Canva()
-{
-}
+Canva::~Canva() {}
+int Canva::getaltura() { return this->altura; }
+int Canva::getlargura() { return this->largura; }
+char **Canva::getpixels() { return this->pixels; }
+char **Canva::getcores() { return this->cores; }
+void Canva::setaltura(int altura) { this->altura = altura; }
+void Canva::setlargura(int largura) { this->largura = largura; }
+void Canva::setpixels(char pixels) { this->pixels = **pixels; }
+void Canva::setcores(char cores) { this->cores = **cores; }
 
-void Canva::CriarCanva(Canva &canva, int altura, int largura)
-{
-  if (altura <= 0 || largura <= 0)
-  {
+void CriarCanva(Canva &canva, int altura, int largura) {
+  if (altura <= 0 || largura <= 0) {
     std::cout << "Error: Altura ou Largura invalidas" << std::endl;
     return;
-  }
-  else
-  {
-    _altura = altura;
-    _largura = largura;
-    _pixels = new char *[_altura];
-    _cores = new char *[_altura];
-    for (int i = 0; i < _altura; i++)
-    {
-      _pixels[i] = new char[_largura];
-      _cores[i] = new char[_largura];
-      for (int j = 0; j < _largura; j++)
-      {
-        _pixels[i][j] = ' ';
-        _cores[i][j] = 'W';
+  } else {
+    canva.setaltura(altura);
+    canva.setlargura(largura);
+    pixels = new char *[altura];
+    cores = new char *[altura];
+    for (int i = 0; i < altura; i++) {
+      pixels[i] = new char[largura];
+      cores[i] = new char[largura];
+      for (int j = 0; j < largura; j++) {
+        canva.setpixels(' ');
+        canva.setcores('W');
       }
     }
   }
 }
 
-void Canva::DestruirCanvas(Canva &canva)
-{
-  if (canva._pixels != nullptr)
-  {
-    for (int i = 0; i < canva._altura; i++)
-    {
+void DestruirCanvas(Canva &canva) {
+  if (canva.getpixels != nullptr) {
+    for (int i = 0; i < canva._altura; i++) {
       delete[] canva._pixels[i];
     }
     delete[] canva._pixels;
     canva._pixels = nullptr;
   }
 
-  if (canva._cores != nullptr)
-  {
-    for (int i = 0; i < canva._altura; i++)
-    {
+  if (canva._cores != nullptr) {
+    for (int i = 0; i < canva._altura; i++) {
       delete[] canva._cores[i];
     }
     delete[] canva._cores;
@@ -85,59 +77,43 @@ void Canva::DestruirCanvas(Canva &canva)
   canva._altura = 0;
   canva._largura = 0;
 }
-void Canva::ImprimirCanva(Canva &Canva)
-{
+void ImprimirCanva(Canva &Canva) {
   std::cout << "+";
-  for (int i = 0; i < _largura; i++)
-  {
+  for (int i = 0; i < _largura; i++) {
     std::cout << "-";
   }
   std::cout << "+" << std::endl;
-  for (int i = 0; i < _altura; i++)
-  {
+  for (int i = 0; i < _altura; i++) {
     std::cout << "|";
-    for (int j = 0; j < _largura; j++)
-    {
+    for (int j = 0; j < _largura; j++) {
       std::cout << GetColorCode(Canva._cores[i][j]);
       std::cout << Canva._pixels[i][j];
     }
     std::cout << "|" << std::endl;
   }
   std::cout << "+";
-  for (int i = 0; i < _largura; i++)
-  {
+  for (int i = 0; i < _largura; i++) {
     std::cout << "-";
   }
   std::cout << "+" << std::endl;
 }
-void Canva::DesenharPonto(Canva &Canva, int x, int y, char pixel, char cores)
-{
-  if (x > 0 && x < Canva._largura && y > 0 && y < Canva._altura)
-  {
+void DesenharPonto(Canva &Canva, int x, int y, char pixel, char cores) {
+  if (x > 0 && x < Canva._largura && y > 0 && y < Canva._altura) {
     Canva._cores[x][y] = cores;
     Canva._pixels[x][y] = pixel;
-  }
-  else
-  {
+  } else {
     std::cout << "X ou Y estao fora do limite do Canvas!" << std::endl;
   }
 }
-void Canva::DesenharLinha(Canva &Canva, int x1, int y1, int x2, int y2, char pixel,
-                          char cores)
-{
-  for (int i = x1; i < x2; i++)
-  {
-    for (int j = y1; j < y2; j++)
-    {
+void DesenharLinha(Canva &Canva, int x1, int y1, int x2, int y2, char pixel,
+                   char cores) {
+  for (int i = x1; i < x2; i++) {
+    for (int j = y1; j < y2; j++) {
       Canva._pixels[i][j] = pixel;
       Canva._cores[i][j] = cores;
     }
   }
 }
-void Canva::DesenharRetangulo(Canva &Canva, int x_superior_esquerdo,
-                              int y_superior_esquerdo, int largura, int altura,
-                              char pixel, char cores)
-{
-
-  
-}
+void DesenharRetangulo(Canva &Canva, int x_superior_esquerdo,
+                       int y_superior_esquerdo, int largura, int altura,
+                       char pixel, char cores) {}
--- Canvas.hpp
+++ Canvas.hpp
@@ -1,32 +1,39 @@
 #ifndef CANVAS_HPP
 #define CANVAS_HPP
-#include <iostream>
 
 class Canva {
 private:
-  int _largura;
-  int _altura;
-  char **_pixels;
-  char **_cores;
+  int largura;
+  int altura;
+  char **pixels;
+  char **cores;
 
 public:
   Canva();
   ~Canva();
-  void CriarCanva(Canva &Canva, int altura, int largura);
-  void DestruirCanvas(Canva &Canva);
-  void ImprimirCanva(Canva &Canva);
-  void DesenharPonto(Canva &Canva, int x, int y, char pixel, char cores);
-  void DesenharLinha(Canva &Canva, int x1, int y1, int x2, int y2, char pixel,
-                     char cores);
-  void DesenharRetangulo(Canva &Canva, int x_superior_esquerdo,
-                         int y_superior_esquerdo, int largura, int altura,
-                         char pixel, char cores);
-  void DesenharRetanguloPreenchido(Canva &Canva, int x_superior_esquerdo,
-                                   int y_superior_esquerdo, int largura,
-                                   int altura, char pixel, char cores);
-  void RedimensionarCanva(Canva &Canva, int novaLargura, int novaAltura);
-  void SobreporCanvas(Canva &Canva_destino, Canva Origem1, Canva Origem2);
-  void CompararCanvas(Canva &Canva_comparar, Canva Conva_comparador);
+  int getlargura();
+  int getaltura();
+  char getpixels();
+  char getcores();
+  void setlargura(int);
+  void setaltura(int);
+  void setpixels(char);
+  void setcores(char);
 };
 
+void CriarCanva(Canva &Canva, int altura, int largura);
+void DestruirCanvas(Canva &Canva);
+void ImprimirCanva(Canva &Canva);
+void DesenharPonto(Canva &Canva, int x, int y, char pixel, char cores);
+void DesenharLinha(Canva &Canva, int x1, int y1, int x2, int y2, char pixel,
+                   char cores);
+void DesenharRetangulo(Canva &Canva, int x_superior_esquerdo,
+                       int y_superior_esquerdo, int largura, int altura,
+                       char pixel, char cores);
+void DesenharRetanguloPreenchido(Canva &Canva, int x_superior_esquerdo,
+                                 int y_superior_esquerdo, int largura,
+                                 int altura, char pixel, char cores);
+void RedimensionarCanva(Canva &Canva, int novaLargura, int novaAltura);
+void SobreporCanvas(Canva &Canva_destino, Canva Origem1, Canva Origem2);
+void CompararCanvas(Canva &Canva_comparar, Canva Conva_comparador);
 #endif
